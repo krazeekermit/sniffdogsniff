86b7aa654e9c118422d5390f85953663eee6ff61
krazeekermit <krazeekermit@proton.me>
2023-09-21 22:36:23 +0200

    new logging

--- logging/logging.go
+++ logging/logging.go
@@ -1,10 +1,12 @@
 package logging
 
 import (
+	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
 const (
@@ -12,6 +14,13 @@ const (
 	WARN  int = 1
 	ERROR int = 2
 	TRACE int = 3
+)
+
+const (
+	INFO_STR  = "info"
+	WARN_STR  = "warn"
+	ERROR_STR = "error"
+	TRACE_STR = "trace"
 )
 
 const (
@@ -23,16 +32,19 @@ const (
 )
 
 var logLevel int
+var noAnsi bool = false
+var mutex *sync.Mutex
+var outStream *bufio.Writer
 
 func StrToLogLevel(levelStr string) int {
-	switch strings.ToUpper(levelStr) {
-	case "INFO":
+	switch strings.ToLower(levelStr) {
+	case INFO_STR:
 		return INFO
-	case "WARN":
+	case WARN_STR:
 		return WARN
-	case "ERROR":
+	case ERROR_STR:
 		return ERROR
-	case "TRACE":
+	case TRACE_STR:
 		return TRACE
 	}
 	return TRACE
@@ -46,35 +58,56 @@ func InitLogging(level int) {
 	logLevel = level
 }
 
+func printlog(level, color, message string) {
+	if mutex == nil {
+		mutex = &sync.Mutex{}
+	}
+	if outStream == nil {
+		outStream = bufio.NewWriter(os.Stdout)
+	}
+
+	end := ANSI_END
+	if noAnsi {
+		color = ""
+		end = ""
+	}
+
+	mutex.Lock()
+	fmt.Fprintf(outStream, "[%s %s%5s%s] %s\n", time.Now().Local().Format(time.ANSIC), color, level, end, message)
+	outStream.Flush()
+	mutex.Unlock()
+}
+
 func SetLoggingToFile(path string) {
 	fp, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
 		panic("failed to create log file")
 	}
 
-	log.SetOutput(fp)
+	noAnsi = true
+	outStream = bufio.NewWriter(fp)
 }
 
 func LogInfo(a ...any) {
 	if logLevel >= INFO {
-		log.Println(" [INFO] ", SprintTrimmed(a...))
+		printlog(INFO_STR, ANSI_END, SprintTrimmed(a...))
 	}
 }
 
 func LogWarn(a ...any) {
 	if logLevel >= WARN {
-		log.Println(ANSI_YELLOW, "[WARN] ", SprintTrimmed(a...), ANSI_END)
+		printlog(WARN_STR, ANSI_YELLOW, SprintTrimmed(a...))
 	}
 }
 
 func LogError(a ...any) {
 	if logLevel >= ERROR {
-		log.Println(ANSI_RED, "[ERROR]", SprintTrimmed(a...), ANSI_END)
+		printlog(ERROR_STR, ANSI_RED, SprintTrimmed(a...))
 	}
 }
 
 func LogTrace(a ...any) {
 	if logLevel >= TRACE {
-		log.Println(ANSI_CYAN, "[TRACE]", SprintTrimmed(a...), ANSI_END)
+		printlog(TRACE_STR, ANSI_CYAN, SprintTrimmed(a...))
 	}
 }
